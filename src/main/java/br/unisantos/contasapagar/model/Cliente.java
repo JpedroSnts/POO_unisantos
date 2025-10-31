68e00cf5949f466b246e03be282894c78c004b53
João Pedro <jp.siq.snts@gmail.com>
2025-10-31 20:29:53 -0300

    Arquitetada a classe Produto :)

--- src/main/java/br/unisantos/contasapagar/model/Cliente.java
+++ src/main/java/br/unisantos/contasapagar/model/Cliente.java
@@ -2,18 +2,10 @@ package br.unisantos.contasapagar.model;
 
 public class Cliente {
 	
-	private Long id;
 	private String nome;
 	private String email;
 	private String telefone;
-	private String cpf;
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
+
 	public String getNome() {
 		return nome;
 	}
