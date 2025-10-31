2e54e1449e3630ec418cc73efcfa50984a6e47ec
João Pedro <jp.siq.snts@gmail.com>
2025-10-31 19:54:03 -0300

    commit inicial

---
+++ src/test/java/br/unisantos/contasapagar/ContasapagarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unisantos.contasapagar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContasapagarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
